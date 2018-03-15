ca7e437700b3bf3eba3240484a789728a48bd17d
Baghdasaryan <Vagharshak.Baghdasaryan@bonprix.net>
2018-03-15 16:00:30 +0100

    FW - 619: objectId not necessary, if combo box caprion is generated via ItemCaptionGenerator

--- GridEnhancements/src/main/java/org/vaadin/grid/enhancements/filter/ItemCaptionGeneratorFilter.java
+++ GridEnhancements/src/main/java/org/vaadin/grid/enhancements/filter/ItemCaptionGeneratorFilter.java
@@ -19,7 +19,6 @@ import java.util.function.Function;
 
 import com.vaadin.data.Container.Filter;
 import com.vaadin.data.Item;
-import com.vaadin.data.Property;
 import com.vaadin.data.util.BeanItemContainer;
 
 /**
@@ -28,122 +27,112 @@ import com.vaadin.data.util.BeanItemContainer;
  */
 public final class ItemCaptionGeneratorFilter<BEANTYPE> implements Filter {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	final Function<BEANTYPE, String> itemCaptionGenerator;
-	final Object propertyId;
-	final String filterString;
-	final boolean ignoreCase;
-	final boolean onlyMatchPrefix;
-	final BeanItemContainer<BEANTYPE> container;
+    final transient Function<BEANTYPE, String> itemCaptionGenerator;
+    final String filterString;
+    final boolean ignoreCase;
+    final boolean onlyMatchPrefix;
+    final BeanItemContainer<BEANTYPE> container;
 
-	public ItemCaptionGeneratorFilter(final Function<BEANTYPE, String> itemCaptionGenerator, Object propertyId,
-			final String filterString, final boolean ignoreCase, final boolean onlyMatchPrefix,
-			final BeanItemContainer<BEANTYPE> container) {
-		this.itemCaptionGenerator = itemCaptionGenerator;
-		this.propertyId = propertyId;
-		this.filterString = ignoreCase ? filterString.toLowerCase() : filterString;
-		this.ignoreCase = ignoreCase;
-		this.onlyMatchPrefix = onlyMatchPrefix;
-		this.container = container;
-	}
+    public ItemCaptionGeneratorFilter(final Function<BEANTYPE, String> itemCaptionGenerator, final String filterString, final boolean ignoreCase,
+            final boolean onlyMatchPrefix, final BeanItemContainer<BEANTYPE> container) {
+        this.itemCaptionGenerator = itemCaptionGenerator;
+        this.filterString = ignoreCase ? filterString.toLowerCase() : filterString;
+        this.ignoreCase = ignoreCase;
+        this.onlyMatchPrefix = onlyMatchPrefix;
+        this.container = container;
+    }
 
-	@Override
-	public boolean passesFilter(final Object itemId, final Item item) {
-		final Property<?> idProperty = item.getItemProperty(this.propertyId);
-		if (idProperty.getValue() == null) {
-			return false;
-		}
+    @Override
+    public boolean passesFilter(final Object itemId, final Item item) {
+        if (this.itemCaptionGenerator == null) {
+            return false;
+        }
 
-		String value = this.itemCaptionGenerator.apply((BEANTYPE) itemId);
-		value = this.ignoreCase ? value.toString()
-			.toLowerCase() : value.toString();
-		if (this.onlyMatchPrefix) {
-			if (!value.startsWith(this.filterString)) {
-				return false;
-			}
-		} else {
-			if (!value.contains(this.filterString)) {
-				return false;
-			}
-		}
-		return true;
-	}
+        String value = this.itemCaptionGenerator.apply((BEANTYPE) itemId);
+        value = this.ignoreCase ? value.toLowerCase() : value;
+        if (this.onlyMatchPrefix) {
+            if (!value.startsWith(this.filterString)) {
+                return false;
+            }
+        }
+        else {
+            if (!value.contains(this.filterString)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public boolean appliesToProperty(final Object propertyId) {
-		return true;
-	}
+    @Override
+    public boolean appliesToProperty(final Object propertyId) {
+        return true;
+    }
 
-	@Override
-	public boolean equals(final Object obj) {
-		if (obj == null) {
-			return false;
-		}
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
 
-		// Only ones of the objects of the same class can be equal
-		if (!(obj instanceof ItemCaptionGeneratorFilter)) {
-			return false;
-		}
-		@SuppressWarnings("unchecked")
-		final ItemCaptionGeneratorFilter<BEANTYPE> o = (ItemCaptionGeneratorFilter<BEANTYPE>) obj;
+        // Only ones of the objects of the same class can be equal
+        if (!(obj instanceof ItemCaptionGeneratorFilter)) {
+            return false;
+        }
+        @SuppressWarnings("unchecked")
+        final ItemCaptionGeneratorFilter<BEANTYPE> o = (ItemCaptionGeneratorFilter<BEANTYPE>) obj;
 
-		// Checks the properties one by one
-		if (this.filterString != o.filterString && o.filterString != null
-				&& !o.filterString.equals(this.filterString)) {
-			return false;
-		}
-		if (this.ignoreCase != o.ignoreCase) {
-			return false;
-		}
-		if (this.onlyMatchPrefix != o.onlyMatchPrefix) {
-			return false;
-		}
+        // Checks the properties one by one
+        if (this.filterString != o.filterString && o.filterString != null && !o.filterString.equals(this.filterString)) {
+            return false;
+        }
+        if (this.ignoreCase != o.ignoreCase) {
+            return false;
+        }
 
-		return true;
-	}
+        if (this.onlyMatchPrefix != o.onlyMatchPrefix) {
+            return false;
+        }
 
-	@Override
-	public int hashCode() {
-		return (this.filterString != null ? this.filterString.hashCode() : 0);
-	}
+        return true;
+    }
 
-	/**
-	 * Returns the filter string.
-	 *
-	 * Note: this method is intended only for implementations of lazy string
-	 * filters and may change in the future.
-	 *
-	 * @return filter string given to the constructor
-	 */
-	public String getFilterString() {
-		return this.filterString;
-	}
+    @Override
+    public int hashCode() {
+        return (this.filterString != null ? this.filterString.hashCode() : 0);
+    }
 
-	/**
-	 * Returns whether the filter is case-insensitive or case-sensitive.
-	 *
-	 * Note: this method is intended only for implementations of lazy string
-	 * filters and may change in the future.
-	 *
-	 * @return true if performing case-insensitive filtering, false for
-	 *         case-sensitive
-	 */
-	public boolean isIgnoreCase() {
-		return this.ignoreCase;
-	}
+    /**
+     * Returns the filter string.
+     *
+     * Note: this method is intended only for implementations of lazy string filters and may change in the future.
+     *
+     * @return filter string given to the constructor
+     */
+    public String getFilterString() {
+        return this.filterString;
+    }
 
-	/**
-	 * Returns true if the filter only applies to the beginning of the value
-	 * string, false for any location in the value.
-	 *
-	 * Note: this method is intended only for implementations of lazy string
-	 * filters and may change in the future.
-	 *
-	 * @return true if checking for matches at the beginning of the value only,
-	 *         false if matching any part of value
-	 */
-	public boolean isOnlyMatchPrefix() {
-		return this.onlyMatchPrefix;
-	}
+    /**
+     * Returns whether the filter is case-insensitive or case-sensitive.
+     *
+     * Note: this method is intended only for implementations of lazy string filters and may change in the future.
+     *
+     * @return true if performing case-insensitive filtering, false for case-sensitive
+     */
+    public boolean isIgnoreCase() {
+        return this.ignoreCase;
+    }
+
+    /**
+     * Returns true if the filter only applies to the beginning of the value string, false for any location in the value.
+     *
+     * Note: this method is intended only for implementations of lazy string filters and may change in the future.
+     *
+     * @return true if checking for matches at the beginning of the value only, false if matching any part of value
+     */
+    public boolean isOnlyMatchPrefix() {
+        return this.onlyMatchPrefix;
+    }
 }
